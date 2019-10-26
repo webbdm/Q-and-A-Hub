ed11c5bb1ac52554b109601692a6f55b3bedeb3c
Dominic Serrano <dserrano@atiba.com>
2019-10-26 12:47:20 -0500

    Setup eslintrc.js

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Question extends Component {
-    render() {
-        return <div>Question</div>;
-    }
-};
+	render() {
+		return <div>Question</div>;
+	}
+}
 
 export default Question;
