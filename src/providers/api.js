4df5416922676b8922ba52adea0f679e738dc2fe
Dominic Serrano <dserrano@atiba.com>
2019-10-26 13:26:00 -0500

    Eslint errors

--- src/providers/api.js
+++ src/providers/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://localhost:4000';
+const baseURL = "http://localhost:4000";
 
 export const api = {
-    get: url => axios.get(`${baseURL}` + `${url}`),
-    post: url => axios.post(`baseURL` + `${url}`),
-    put: url => axios.put(`baseURL` + `${url}`),
-    delete: url => axios.delete`baseURL` + `${url}`
-}
+	get: url => axios.get(`${baseURL}` + `${url}`),
+	post: url => axios.post("baseURL" + `${url}`),
+	put: url => axios.put("baseURL" + `${url}`),
+	delete: url => axios.delete`baseURL` + `${url}`
+};
