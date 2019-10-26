3001dd637c08224f2225cf4a4e5a5cdfe2c07511
webbdm <gwebb762@gmail.com>
2019-10-26 16:37:40 -0500

    Fetch answers with questions

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,23 +1,56 @@
 import React, { Component } from "react";
 
+import { questionsApi, answersApi } from "../../providers/api";
+
 import './Question.scss';
 
-const QuestionCard = () => <div className="question">
+const Answer = ({ text }) => <h1>{text}</h1>;
+
+const QuestionCard = ({ text, answers }) => <div className="question">
 	<div className="card">
 		<div className="card-header">
-			Why Does X do Y?
-  </div>
+			{text}
+		</div>
 		<div className="card-body">
-
+			{answers && answers.map(answer => <Answer key={answer.id} text={answer.text} />)}
 		</div>
 	</div>
 </div>
 
 class Question extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			questions: []
+		};
+	}
+
+	// TODO: make this work with API
+	getQuestions() {
+		return questionsApi.get()
+			.then(({ data }) => {
+				const qIds = data.map(q => answersApi.get({ params: { question_id: q.id } }));
+
+				this.setState({ questions: data });
+
+				return Promise.all([data, ...qIds]);
+
+			}).then(([questions, ...answers]) => {
+				const ans = answers.map(a => a.data[0]);
+				const qs = this.state.questions;
+				this.setState({ questions: qs.map(q => ({ ...q, answers: ans.filter(a => a.question_id === q.id) })) });
+
+			});
+	}
+
+	componentDidMount() {
+		this.getQuestions();
+	}
 	render() {
 		return (
 			<div className="question-wrapper">
-				<QuestionCard />
+				{this.state.questions.map(question => <QuestionCard key={question.id} answers={question.answers} text={question.text} />)}
 			</div>
 		);
 	}
